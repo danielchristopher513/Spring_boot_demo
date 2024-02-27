f293186a25ab19e628bde9ef979132720b37de6f
J P DANIEL CHRISTOPHER <danielchristopher513@gmail.com>
2024-02-27 16:08:31 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/RestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import  java.lang.*;
+@SpringBootApplication
+public class RestSpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestSpringBootApplication.class, args);
+	}
+}
